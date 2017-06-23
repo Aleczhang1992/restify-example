c8934bcd440e54f7b7f4de31197b6c01402a272a
aleczhang <aleczhang@163.com>
2017-06-23 15:28:47 +0800

    。。

--- models/index.js
+++ models/index.js
@@ -8,7 +8,9 @@ const logger = require('../utils/logging');
 
 //mongoose.connect(nconf.get('Mongodb:Mongo'), function (err) {
 	//env.MONGO_PORT_27017_TCP_ADDR是在.bash_profile中配置的环境变量
-mongoose.connect('mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':27017/test_server', function (err) {
+//	+process.env.MONGO_PORT_27017_TCP_ADDR+
+	
+mongoose.connect('mongodb://127.0.0.1:27017/test_server', function (err) {
     if (err) {
         logger.dbLogger.info('connect to %s error: ', nconf.get('Mongodb:Name'), err.message);
         process.exit(1);
